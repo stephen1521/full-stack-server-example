aa08a112ca82e7677c8d2f763c77285002c83738
dd-code-immersives <domilamad89@gmail.com>
2023-03-13 12:52:50 -0400

    added /api/books route

--- routes/api/books.js
+++ routes/api/books.js
@@ -9,5 +9,13 @@ const Book = require('../../models/Book')
 // test route, to make sure API routing is running 
 router.get('/test', (req,res) => res.send("book route testing"))
 
+// show all the books
+router.get('/', (req, res) => {
+    Book.find()
+    .then(books => res.json(books))
+    .catch(err => res.status(404).json({
+        nobooksfound: 'no books found'
+    }))
+})
 
 module.exports = router;
