2c858a882722f7baee1bbd0fe2056967feda1ccb
dd-code-immersives <domilamad89@gmail.com>
2023-03-13 15:25:48 -0400

    added middleware and cors policy

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 //load environment variables in .env file
 require('dotenv').config();
@@ -14,16 +15,26 @@ const books = require('./routes/api/books');
 //instantiates express
 const app = express();
 
+//connect to the DB 
+connectDB();
+
+//allow cross origin requests (this is for production)
+//production code = code that hasn't been released
+app.use(cors({ origin: true, credentials: true}))
+
+//middle ware 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 //welcome page 
 app.get('/', (req, res) => res.send("Hello World!"));
 
-// use books route
+// use books routes
 app.use('/api/books', books);
 
 
-//connect to the DB 
-connectDB();
+
+
 
 //sets the port defaults to 8000
 const port = process.env.PORT || 8000
