00fab8a876df2af065aad8dbe2fc2ca554eab229
dd-code-immersives <domilamad89@gmail.com>
2023-03-13 12:04:42 -0400

    removed uneeded options from mongoose

--- config/db.js
+++ config/db.js
@@ -5,8 +5,10 @@ const db = process.env.MONGO_URI
 const connectDB = async () => {
 
     try {
+        //strictQuery, will check Schema for property before running
+        // this is more efficient 
         mongoose.set('strictQuery', true);
-        await mongoose.connect(db, { useNewUrlParser: true})
+        await mongoose.connect(db)
     } catch (error) {
         console.error(error.message);
         process.exit(1)
